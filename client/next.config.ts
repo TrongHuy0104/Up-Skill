fc4e535d2cb07918e196894a5be559bf55487df5
huy01040104 <tronghuy01040@gmail.com>
2025-01-26 09:12:11 +0700

    fix: add enviroment for build client

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,16 @@
 const nextConfig = {
     experimental: {
         appDir: true // Ensure the app directory is enabled
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/a/**'
+            }
+        ]
     }
 };
 
