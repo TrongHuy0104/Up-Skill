c7b0cea1effba8f77ff9127fea824b1a6a20202e
congxdanh <danh.hsb13@gmail.com>
2025-02-16 14:20:05 +0700

    fix: update UI component

--- client/src/app/instructor-list/_component/ModalComponent.tsx
+++ client/src/app/instructor-list/_component/ModalComponent.tsx
@@ -5,58 +5,40 @@ const ModalComponent = ({
     closeModal: () => void;
     onSelectSort: (sortValue: string) => void;
 }) => (
-    <div className="absolute top-[60%] right-0 mt-2 bg-white rounded-lg shadow-lg w-48 p-4 z-50">
+    <div className="absolute top-[60%] right-0 mt-2 bg-white rounded-lg shadow-lg w-40 p-4 z-50">
         <ul className="space-y-2">
-            <li
-                className="cursor-pointer hover:bg-gray-200 p-2 rounded"
-                onClick={() => {
-                    onSelectSort('Best Selling');
-                    closeModal();
-                }}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter' || e.key === ' ') {
+            <li>
+                <button
+                    className="w-full cursor-pointer hover:bg-gray-200 p-2 rounded"
+                    onClick={() => {
                         onSelectSort('Best Selling');
                         closeModal();
-                    }
-                }}
-                tabIndex={0} // Làm cho phần tử có thể nhận sự kiện bàn phím
-                role="button" // Đảm bảo phần tử <li> được nhận diện là button cho accessibility
-            >
-                Best Selling
+                    }}
+                >
+                    Best Selling
+                </button>
             </li>
-            <li
-                className="cursor-pointer hover:bg-gray-200 p-2 rounded"
-                onClick={() => {
-                    onSelectSort('Oldest');
-                    closeModal();
-                }}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter' || e.key === ' ') {
+            <li>
+                <button
+                    className="w-full cursor-pointer hover:bg-gray-200 p-2 rounded"
+                    onClick={() => {
                         onSelectSort('Oldest');
                         closeModal();
-                    }
-                }}
-                tabIndex={0} // Làm cho phần tử có thể nhận sự kiện bàn phím
-                role="button" // Đảm bảo phần tử <li> được nhận diện là button cho accessibility
-            >
-                Oldest
+                    }}
+                >
+                    Oldest
+                </button>
             </li>
-            <li
-                className="cursor-pointer hover:bg-gray-200 p-2 rounded"
-                onClick={() => {
-                    onSelectSort('3 days');
-                    closeModal();
-                }}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter' || e.key === ' ') {
+            <li>
+                <button
+                    className="w-full cursor-pointer hover:bg-gray-200 p-2 rounded"
+                    onClick={() => {
                         onSelectSort('3 days');
                         closeModal();
-                    }
-                }}
-                tabIndex={0} // Làm cho phần tử có thể nhận sự kiện bàn phím
-                role="button" // Đảm bảo phần tử <li> được nhận diện là button cho accessibility
-            >
-                3 days
+                    }}
+                >
+                    3 days
+                </button>
             </li>
         </ul>
     </div>
