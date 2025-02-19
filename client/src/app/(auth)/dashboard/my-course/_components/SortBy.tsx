7897fb978e960d0a593ab5dedaf2bc020214d482
Lê Nguyễn Phúc Anh <anhphuc@MacBook-Air-cua-Le.local>
2025-02-19 07:55:18 +0700

    fix: commit fail  Intructor course dashboard page, component SortBy

--- client/src/app/(auth)/dashboard/my-course/_components/SortBy.tsx
+++ client/src/app/(auth)/dashboard/my-course/_components/SortBy.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import arrowDownIcon from '@/public/assets/icons/arrow-down.svg';
 import { useState } from "react";
 
+// Mark props as readonly
 interface SortByProps {
-    options: { value: string; label: string }[];
-    defaultValue?: string;
+    readonly options: readonly { value: string; label: string }[];
+    readonly defaultValue?: string;
 }
 
 export default function SortBy({ options, defaultValue }: SortByProps) {
@@ -15,23 +16,22 @@ export default function SortBy({ options, defaultValue }: SortByProps) {
     return (
         <div className="mt-[-8px]">
             <div className="flex flex-wrap items-center text-[15px]">
-                <p
-                    className="text-[15px] px-[7px] pl-10 text-primary-600 font-normal leading-[28px]"
-                >
+                <p className="text-[15px] px-[7px] pl-10 text-primary-600 font-normal leading-[28px]">
                     Sort by
                 </p>
-                <div
-                    className="flex relative cursor-pointer text-primary-800 py-4 w-48 "
+                <div 
+                    className="flex relative cursor-pointer text-primary-800 py-4 w-48"
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     <span className="block pr-4">{selected}</span>
+                    
                     {isOpen && (
                         <ul className="absolute left-0 top-full mt-1 w-full bg-white border border-gray-300 rounded-lg shadow-lg">
                             {options.map((option) => (
                                 <li
                                     key={option.value}
-                                    className={`px-4 py-2 cursor-pointer hover:bg-gray-100 ${selected === option.label ? "text-accent-900 " : ""
-                                        }`}
+                                    className={`px-4 py-2 cursor-pointer hover:bg-gray-100 
+                                        ${selected === option.label ? "text-accent-900" : ""}`}
                                     onClick={() => {
                                         setSelected(option.label);
                                         setIsOpen(false);
@@ -42,10 +42,8 @@ export default function SortBy({ options, defaultValue }: SortByProps) {
                             ))}
                         </ul>
                     )}
-                    <Image src={arrowDownIcon} alt="arrow down icon"
-                    />
+                    <Image src={arrowDownIcon} alt="arrow down icon" />
                 </div>
-
             </div>
         </div>
     );
