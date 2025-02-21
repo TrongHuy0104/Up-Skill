cc519c27b47f2b5516c083b0f0b72c5886cbfc81
thanglp163 <lethangtty2018@gmail.com>
2025-02-21 22:41:56 +0700

    feat: instructor single page

---
+++ client/src/app/instructor-single/_components/CoursesList.tsx
@@ -0,0 +1,15 @@
+import CourseVerticalCard from '@/components/custom/CourseCard';
+import React from 'react';
+
+export default function CoursesList() {
+    return (
+        <div className="mb-[61px] w-[9000px]">
+            <h2 className="text-2xl font-bold mb-4">My courses (29)</h2>
+            <div className="flex gap-7">
+                <CourseVerticalCard />
+                <CourseVerticalCard />
+                <CourseVerticalCard />
+            </div>
+        </div>
+    );
+}
---
+++ client/src/app/instructor-single/_components/InstructorInfor.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+const InstructorInfor = () => {
+    return (
+        <div className="w-full text-primary-800 ">
+            <div className="pb-[61px] leading-7 w-[900px]">
+                <h2 className="text-2xl font-bold mb-4">About Me</h2>
+
+                <div className="">
+                    <p>
+                        Lorem ipsum dolor sit amet consectur adipisicing elit, sed do eiusmod tempor inc idid unt ut
+                        labore et dolore magna aliqua enim ad minim veniam, quis nostrud exerec tation ullamco laboris
+                        nis aliquip commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur enim ipsam.
+                    </p>
+                    <p className="mt-4">
+                        Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id
+                        est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                        doloremque laudantium totam rem aperiam.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InstructorInfor;
