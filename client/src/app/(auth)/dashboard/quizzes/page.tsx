7a72c129e125e831613cc919aa1b08992a823a5c
congxdanh <danh.hsb13@gmail.com>
2025-03-02 09:20:23 +0700

    feat: base FE quiz to question

--- client/src/app/(auth)/dashboard/quizzes/page.tsx
+++ client/src/app/(auth)/dashboard/quizzes/page.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { Suspense } from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs';
+import { DashboardSkeleton } from '@/components/ui/Skeleton';
+import Question from './quizzes/[quizId]/question/_components/Question';
+import Quiz from './quizzes/[quizId]/_components/Quiz';
 
 export default function page() {
-    return <div>page quiz</div>;
+    return (
+        <>
+            <div className="pt-8 px-10 pb-10 ml-auto max-w-[1000px] border border-primary-100 rounded-xl">
+                <Suspense fallback={<DashboardSkeleton />}>
+                    <Tabs defaultValue="profile">
+                        <TabsList>
+                            <TabsTrigger value="quiz">Quiz</TabsTrigger>
+                            <TabsTrigger value="question">Question</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="quiz">
+                            <Quiz />
+                        </TabsContent>
+                        <TabsContent value="question">
+                            <Question />
+                        </TabsContent>
+                    </Tabs>
+                </Suspense>
+            </div>
+        </>
+    );
 }
