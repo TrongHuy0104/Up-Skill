be5b3dd4443da065edf760e904deb9760884cebe
Le Thang <thanglpqe17122@fpt.edu.vn>
2025-03-12 11:38:11 +0700

    feat: dashboard

--- client/src/app/(auth)/admin/_components/SideBar.tsx
+++ client/src/app/(auth)/admin/_components/SideBar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import manageUser from '@/public/assets/icons/students.svg';
 import manageCourse from '@/public/assets/icons/timetable.svg';
 import dashboard from '@/public/assets/icons/dashboard.svg';
+import manageInstructor from '@/public/assets/icons/students.svg';
 
 const menuItems = [
     {
@@ -15,7 +16,8 @@ const menuItems = [
     {
         title: 'DATA',
         items: [
-            { icon: manageUser, label: 'Manage User', path: '/admin/users' },
+            { icon: manageUser, label: 'User', path: '/admin/users' },
+            { icon: manageInstructor, label: 'Instructors', path: '/admin/instructors' },
             { icon: manageCourse, label: 'Manage Course', path: '/admin/courses' }
         ]
     }
@@ -29,7 +31,7 @@ const Sidebar = () => {
             {/* Logo */}
             <div className=" flex items-center p-4 gap-4">
                 <Image src="/assets/images/logo/favicon.png" alt="Logo" width={50} height={50} />
-                <div className="text-gray-400 text-2xl">UpSkill</div>
+                <div className="text-white text-2xl">UpSkill</div>
             </div>
             {/* Avatar and Role */}
             <div className="mt-4 text-white px-4 py-8 flex flex-col items-center gap-2">
@@ -40,21 +42,21 @@ const Sidebar = () => {
                     height={50}
                     className="rounded-full"
                 />
-                <div className='text-2xl'>tên</div>
+                <div className="text-2xl">tên</div>
                 <div>role</div>
             </div>
 
             {/* Menu */}
-            <div className="w-full max-h-screen overflow-y-auto overflow-x-hidden p-4">
+            <div className="w-full max-h-screen overflow-y-auto overflow-x-hidden p-4 text-white">
                 {menuItems.map((i) => (
                     <div className="flex flex-col gap-2 w-full" key={i.title}>
-                        <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+                        <span className="hidden lg:block font-light my-4">{i.title}</span>
                         {i.items.map((item) => (
                             <Link
                                 href={item.path}
                                 key={item.label}
-                                className={`flex items-center justify-center lg:justify-start gap-4 text-gray-400 py-2 rounded-md ${
-                                    pathname === item.path ? 'text-[#4845AA]' : ''
+                                className={`flex items-center justify-center lg:justify-start gap-4 py-2 rounded-md ${
+                                    pathname === item.path ? 'bg-[#4845AA]' : ''
                                 }`}
                             >
                                 <Image
---
+++ client/src/app/(auth)/admin/instructors/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListInstructor from './_components/ListInstructor';
+
+function page() {
+    return (
+        <div>
+            <ListInstructor />
+        </div>
+    );
+}
+
+export default page;
