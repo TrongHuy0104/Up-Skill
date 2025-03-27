a0844152c6f0507b173d6dae7f9762b87e7f66d9
thanglp163 <lethangtty2018@gmail.com>
2025-03-27 18:27:55 +0700

    feat: home screen responsive

--- client/src/components/home/Stats.tsx
+++ client/src/components/home/Stats.tsx
@@ -5,41 +5,45 @@ import Image from 'next/image';
 
 function Stats() {
     return (
-        <section className={`${layoutStyles.container} mb-[140px]`}>
+        <section className={`${layoutStyles.container} mb-20 xl:mb-[140px]`}>
             <div className={layoutStyles.row}>
                 <div className="w-full">
-                    <div className="flex">
-                        <div className="w-1/2">
+                    <div className="flex flex-col xl:flex-row">
+                        {/* Phần ảnh */}
+                        <div className="w-full xl:w-1/2">
                             <Image
                                 src={statImg}
-                                alt=""
-                                className="w-full h-full object-cover border rounded-l-[12px]"
+                                alt="Success illustration"
+                                className="w-full h-auto xl:h-full object-cover rounded-t-[12px] xl:rounded-l-[12px] xl:rounded-tr-none"
                             />
                         </div>
-                        <div className="flex flex-col justify-center p-[50px] pr-[80px] pl-[64px] bg-accent-100 rounded-r-[12px] rounded-l-none w-1/2 text-primary-800">
-                            <h2 className="font-bold text-4xl font-cardo mb-2 leading-[50px]">
+                        
+                        {/* Phần nội dung */}
+                        <div className="w-full xl:w-1/2 flex flex-col justify-center p-6 xl:p-[50px] xl:pr-[80px] xl:pl-[64px] bg-accent-100 rounded-b-[12px] xl:rounded-r-[12px] xl:rounded-bl-none">
+                            <h2 className="font-bold text-2xl xl:text-4xl font-cardo mb-2 xl:leading-[50px]">
                                 Learn The Secrets To Life Success, These People Have Got The Key.​
                             </h2>
-                            <p className="text-base leading-7 mb-6">
+                            <p className="text-sm xl:text-base leading-6 xl:leading-7 mb-4 xl:mb-6 px-4 xl:px-4">
                                 Lorem ipsum dolor sit amet consectur adipiscing elit sed eiusmod ex tempor incididunt
                                 labore dolore magna aliquaenim minim.
                             </p>
-                            <div className="flex flex-wrap gap-y-6 gap-x-[30px] justify-start">
-                                <div className="pl-[18px] text-start min-w-[250px] border-l-2 border-accent-900">
-                                    <div className="text-xl leading-[30px] font-medium mb-1">2.5 Billion+</div>
-                                    <span>Qualified Instructor</span>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 xl:gap-y-6 xl:gap-x-[30px] px-4 xl:px-4">
+                                {/* Các items thống kê */}
+                                <div className="pl-3 xl:pl-[18px] text-start border-l-2 border-accent-900">
+                                    <div className="text-lg xl:text-xl leading-[1.5] xl:leading-[30px] font-medium mb-1">2.5 Billion+</div>
+                                    <span className="text-sm xl:text-base">Qualified Instructor</span>
                                 </div>
-                                <div className="pl-[18px] text-start min-w-[250px] border-l-2 border-accent-900">
-                                    <div className="text-xl leading-[30px] font-medium mb-1">458,000+</div>
-                                    <span>Course enrollments</span>
+                                <div className="pl-3 xl:pl-[18px] text-start border-l-2 border-accent-900">
+                                    <div className="text-lg xl:text-xl leading-[1.5] xl:leading-[30px] font-medium mb-1">458,000+</div>
+                                    <span className="text-sm xl:text-base">Course enrollments</span>
                                 </div>
-                                <div className="pl-[18px] text-start min-w-[250px] border-l-2 border-accent-900">
-                                    <div className="text-xl leading-[30px] font-medium mb-1">78,000+</div>
-                                    <span>Courses in 42 languages</span>
+                                <div className="pl-3 xl:pl-[18px] text-start border-l-2 border-accent-900">
+                                    <div className="text-lg xl:text-xl leading-[1.5] xl:leading-[30px] font-medium mb-1">78,000+</div>
+                                    <span className="text-sm xl:text-base">Courses in 42 languages</span>
                                 </div>
-                                <div className="pl-[18px] text-start min-w-[250px] border-l-2 border-accent-900">
-                                    <div className="text-xl leading-[30px] font-medium mb-1">563.000+</div>
-                                    <span>Online Videos</span>
+                                <div className="pl-3 xl:pl-[18px] text-start border-l-2 border-accent-900">
+                                    <div className="text-lg xl:text-xl leading-[1.5] xl:leading-[30px] font-medium mb-1">563.000+</div>
+                                    <span className="text-sm xl:text-base">Online Videos</span>
                                 </div>
                             </div>
                         </div>
--- client/src/components/home/WhyStudy.tsx
+++ client/src/components/home/WhyStudy.tsx
@@ -8,58 +8,69 @@ import whyStudyIcon3 from '@/public/assets/icons/why-study-3.svg';
 
 function WhyStudy() {
     return (
-        <section className={`${layoutStyles.container} pb-[140px]`}>
+        <section className={`${layoutStyles.container} pb-20 md:pb-[140px]`}>
             <div className={layoutStyles.row}>
                 <div className="w-full">
                     <div className="mb-10 text-center text-primary-800">
-                        <h2 className="mb-2 font-bold font-cardo text-4xl leading-[50px]">Why Study With Us?</h2>
-                        <p>Become a valuable expert with UpSkill.</p>
+                        <h2 className="mb-2 font-bold font-cardo text-3xl md:text-4xl leading-[1.3] md:leading-[50px]">
+                            Why Study With Us?
+                        </h2>
+                        <p className="text-sm md:text-base">Become a valuable expert with UpSkill.</p>
                     </div>
                 </div>
             </div>
-            <div className={layoutStyles.row}>
-                <div className="w-1/3">
+            <div className={`${layoutStyles.row} flex flex-col md:flex-row`}>
+                {/* Item 1 */}
+                <div className="w-full md:w-1/3 mb-10 md:mb-0">
                     <div className="text-center">
-                        <div className="mb-[30px] flex justify-center">
-                            <Image src={whyStudyIcon1} alt="" />
+                        <div className="mb-6 md:mb-[30px] flex justify-center">
+                            <Image src={whyStudyIcon1} alt="World Class Teachers" />
                         </div>
                         <div>
-                            <h4 className="text-xl leading-[30px] mb-[14px] font-medium text-primary-800">
+                            <h4 className="text-xl leading-[1.5] mb-3 md:mb-[14px] font-medium text-primary-800">
                                 World Class Teachers
                             </h4>
-                            <p>
-                                What should be the structure of an effective <br />
+                            <p className="text-sm md:text-base px-4 md:px-0">
+                                What should be the structure of an effective <br className="hidden md:block" />
                                 websites and designs.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="w-1/3">
-                    <div className="mb-[42px] flex justify-center">
-                        <Image src={whyStudyIcon2} alt="" />
-                    </div>
-                    <div>
-                        <h4 className="text-xl leading-[30px] mb-[14px] font-medium text-primary-800 text-center">
-                            Global Community
-                        </h4>
-                        <p className="text-center">
-                            What should be the structure of an effective <br />
-                            websites and designs.
-                        </p>
+                
+                {/* Item 2 */}
+                <div className="w-full md:w-1/3 mb-10 md:mb-0">
+                    <div className="text-center">
+                        <div className="mb-6 md:mb-[30px] flex justify-center">
+                            <Image src={whyStudyIcon2} alt="Global Community" />
+                        </div>
+                        <div>
+                            <h4 className="text-xl leading-[1.5] mb-3 md:mb-[14px] font-medium text-primary-800">
+                                Global Community
+                            </h4>
+                            <p className="text-sm md:text-base px-4 md:px-0">
+                                What should be the structure of an effective <br className="hidden md:block" />
+                                websites and designs.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="w-1/3">
-                    <div className="mb-[42px] flex justify-center">
-                        <Image src={whyStudyIcon3} alt="" />
-                    </div>
-                    <div>
-                        <h4 className="text-xl leading-[30px] mb-[14px] font-medium text-primary-800 text-center">
-                            Top Notch Courses
-                        </h4>
-                        <p className="text-center">
-                            What should be the structure of an effective <br />
-                            websites and designs.
-                        </p>
+                
+                {/* Item 3 */}
+                <div className="w-full md:w-1/3">
+                    <div className="text-center">
+                        <div className="mb-6 md:mb-[30px] flex justify-center">
+                            <Image src={whyStudyIcon3} alt="Top Notch Courses" />
+                        </div>
+                        <div>
+                            <h4 className="text-xl leading-[1.5] mb-3 md:mb-[14px] font-medium text-primary-800">
+                                Top Notch Courses
+                            </h4>
+                            <p className="text-sm md:text-base px-4 md:px-0">
+                                What should be the structure of an effective <br className="hidden md:block" />
+                                websites and designs.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
