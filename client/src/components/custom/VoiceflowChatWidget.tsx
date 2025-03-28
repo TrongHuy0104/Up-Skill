1146224cb48ccee923db8aebfec71a3f865d8d47
huy01040104 <tronghuy01040@gmail.com>
2025-03-28 08:34:05 +0700

    refactor: remove console log

--- client/src/components/custom/VoiceflowChatWidget.tsx
+++ client/src/components/custom/VoiceflowChatWidget.tsx
@@ -1,23 +1,22 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function VoiceflowChatWidget() {
     useEffect(() => {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://cdn.voiceflow.com/widget-next/bundle.mjs';
         script.onload = () => {
             (window as any).voiceflow.chat.load({
                 verify: { projectID: process.env.NEXT_PUBLIC_AI_CHATBOX_PROJECT_ID },
-                url: "https://general-runtime.voiceflow.com",
-                versionID: "production",
+                url: 'https://general-runtime.voiceflow.com',
+                versionID: 'production',
                 voice: {
-                    url: "https://runtime-api.voiceflow.com"
+                    url: 'https://runtime-api.voiceflow.com'
                 }
             });
         };
-        console.log('process.env.AI_CHATBOX_PROJECT_ID ', process.env.NEXT_PUBLIC_AI_CHATBOX_PROJECT_ID);
 
         document.body.appendChild(script);
 
