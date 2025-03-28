3dcea6eefd60c808ed52f35d7f3efaae48a65c9b
huy01040104 <tronghuy01040@gmail.com>
2025-03-28 23:08:56 +0700

    refactor: css admin

--- client/src/app/(auth)/admin/layout.tsx
+++ client/src/app/(auth)/admin/layout.tsx
@@ -3,13 +3,11 @@ import Sidebar from './_components/SideBar';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="h-screen flex">
-            {/* LEFT - Menu Sidebar */}
-            <div className="w-1/5 p-4 bg-white">
+        <div className="bg-[#fafafa]">
+            <div className="h-screen bg-white w-[260px] z-50 fixed top-0 left-0 p-4 border-r border-primary-100 border-opacity-50">
                 <Sidebar />
             </div>
-            {/* RIGHT - Nội dung động */}
-            <div className="w-4/5 flex-1 p-4 bg-gray-200 overflow-auto">{children}</div>
+            <div className="ml-[260px] h-full">{children}</div>
         </div>
     );
 }
